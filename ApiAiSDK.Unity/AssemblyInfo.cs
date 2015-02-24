d15913b8064f5cb817360d14579c426eccdb7bcc
Danil Skachkov <danil.skachkov@speaktoit.com>
2015-02-24 15:40:13 +0600

    Change library properties

--- ApiAiSDK.Unity/AssemblyInfo.cs
+++ ApiAiSDK.Unity/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("ApiAiSDK")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ApiAiSDK.Unity")]
+[assembly: AssemblyDescription("Library provides simple programming interface for making text and voice requests to the API.AI service.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Speaktoit")]
 [assembly: AssemblyProduct("API.AI .NET SDK")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
